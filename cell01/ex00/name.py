79366b15bfdfc9a69435946766b8a3937bf96a4c
PhooPwintChoThar <phoopwintchothar1@gmail.com>
2025-06-23 14:36:32 +0700

    added cell2.0

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name="Phoo Pwint"
+last_name="Cho Thar"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name="Phoo Pwint"
+last_name=" Cho Thar"
+whole_name=first_name+last_name
+print(whole_name)
